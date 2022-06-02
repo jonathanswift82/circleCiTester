8f1699bb8fde7b6e4b7d7e846062592b9bfa3508
= <jonathanswift82@gmail.com>
2022-06-02 17:15:11 -0400

    TES-4

--- .circleci/scripts/jira_test.py
+++ .circleci/scripts/jira_test.py
@@ -2,17 +2,14 @@ import sys
 from jira import JIRA
 from github import Github
 
-# pip install PyGithub
-# pip install jira
-
 def github_PRs(gitToken, gitproject):
     g       = Github( gitToken)
     repo    = g.get_repo(gitproject)
     issues  = repo.get_issues(state="closed")
     return issues
 
-def jira_change_status(jira_user,jira_api_token,jira_server_URL, git_token, git_project):
-    if jira_api_token is None or jira_user is None or jira_server_URL is None or git_token is None or git_project is None:
+def jira_change_status(jira_user,jira_api_token, jira_project_name, jira_server_URL, git_token, git_project):
+    if jira_api_token is None or jira_project_name  is None or jira_user is None or jira_server_URL is None or git_token is None or git_project is None:
         exit(13)
     
     options = {
@@ -22,7 +19,7 @@ def jira_change_status(jira_user,jira_api_token,jira_server_URL, git_token, git_
     jira = JIRA(options, basic_auth=(jira_user,jira_api_token) )
     #query uses JQL
     #jira_issues     = jira.search_issues('project = "L Plus Workbench" AND status IN ("PEER REVIEW") ORDER BY issuekey')
-    jira_issues    = jira.search_issues('project = "testingTeam" AND status IN ("PEER REVIEW") ORDER BY issuekey')
+    jira_issues    = jira.search_issues('project = '+jira_project_name+' AND status IN ("PEER REVIEW") ORDER BY issuekey')
     github_issues  = github_PRs(git_token, git_project)
     
     # loop through issues in Jira comparing them to Issues in Github
@@ -38,8 +35,8 @@ def jira_change_status(jira_user,jira_api_token,jira_server_URL, git_token, git_
 
 if __name__ == "__main__":
     args = sys.argv
-    if(len(args) != 6):
+    if(len(args) != 7):
         exit(12)
     for arg in args:
         print(arg)
-    jira_change_status(args[1], args[2], args[3], args[4], args[5])  
+    jira_change_status(args[1], args[2], args[3], args[4], args[5], args[6])  
